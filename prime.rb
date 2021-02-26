50bcc622b6b32ec592fb3db4e94f32f74367ff7c
Jessica Antunes <jessantunes@protonmail.com>
2021-02-26 16:11:14 -0500

    commit tree c0f2647075a5133e37779d060839e61f70b009c8

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(number)
+    if number <= 1
+        return false
+       
+    elsif (2...number).any? { |integer| number % integer  == 0 }
+              return false
+    else
+              return true
+          
+    end
+end
